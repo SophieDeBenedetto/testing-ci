34a700f02e4159342b3969837828c6e403409348
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-09-22 15:08:06 -0400

    process pull request and send status to github

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,37 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_client, only: :event_handler
 
   def event_handler
     payload = JSON.parse(params[:payload])
+    process_pull_request(payload)
+
     branch_name = payload["pull_request"]["head"]["ref"]
     repo_name = payload["pull_request"]["head"]["repo"]["full_name"]
     user = payload["pull_request"]["user"]["login"]
     full_repo = "http://www.github.com/#{repo_name}.git"
     root = Dir.pwd
-    # binding.pry
     system "git clone #{full_repo} #{root}/app/cloned_repo/#{user}"
     system "git checkout #{branch_name}"
     
-    LearnLinter.new("#{root}/app/cloned_repo/#{user}").lint_directory
+    results = LearnLinter.new("#{root}/app/cloned_repo/#{user}", "quiet").lint_directory
 
     # remove repo contents from app/cloned_repo
     system "rm -rf #{root}/app/cloned_repo/#{user}"
   end
+
+
+  private
+    def configure_client
+      @client ||= Octokit::Client.new(:access_token => ENV['octo_token'])
+    end
+
+    def process_pull_request(payload)
+      puts "Processing pull request..."
+      binding.pry
+      @client.create_status(payload["pull_request"]["head"]["repo"]["full_name"], payload["pull_request"]['head']['sha'], 'pending')
+    end
 end
 
 
