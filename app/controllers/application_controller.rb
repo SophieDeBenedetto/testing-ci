59b7cacd76c3515b43a2eb4a97abde2919caeea6
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-09-22 14:04:41 -0400

    correctly linting cloned directory

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,19 +5,24 @@ class ApplicationController < ActionController::Base
 
   def event_handler
     payload = JSON.parse(params[:payload])
-
     branch_name = payload["pull_request"]["head"]["ref"]
     repo_name = payload["pull_request"]["head"]["repo"]["full_name"]
-
-    full_repo = "http://www.github.com/#{repo_name}/#{branch_name}"
+    user = payload["pull_request"]["user"]["login"]
+    full_repo = "http://www.github.com/#{repo_name}.git"
     root = Dir.pwd
-    system "git clone #{full_repo} #{root}/app/cloned_repo"
-    binding.pry
-    LearnLint.lint_directory()
+    # binding.pry
+    system "git clone #{full_repo} #{root}/app/cloned_repo/#{user}"
+    system "git checkout #{branch_name}"
+    
+    LearnLinter.new("#{root}/app/cloned_repo/#{user}").lint_directory
+
+    # remove repo contents from app/cloned_repo
+    system "rm -rf #{root}/app/cloned_repo/#{user}"
+  end
+end
+
 
     # grab the url + branch name from payload hash
     # clone it down onto the server(?)
     # run our LearnLinter.lint_directory(that dir, quiet)
     # use return value of ^^ to send something back to the repo/PR GUI
-  end
-end
