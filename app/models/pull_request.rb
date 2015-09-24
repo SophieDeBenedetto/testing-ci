f7abc876adbea2ffbfb3ef869bb88260c426125c
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-09-24 00:05:13 -0400

    fix pullrequest tests

--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -33,33 +33,33 @@ class PullRequest
     end
   end
 
-  private
+  
 
-    def lint
-      @linter = LearnLinter.new("#{self.root}/app/cloned_repo/#{self.user}", "quiet")
-      @linter.lint_directory
-    end
+  def lint
+    @linter = LearnLinter.new("#{self.root}/app/cloned_repo/#{self.user}", "quiet")
+    @linter.lint_directory
+  end
 
-    def collect_results
-      lint.collect do |file, attributes|
-        attributes.collect do |attr, value|
-          value
-        end
-      end.flatten!
-    end
-
-    def validation_result(result=nil)
-      case result
-      when "failure"  
-        self.status = "failure"
-        self.description[:description] = "failed all validations, click the 'details' link for more info."
-      when "success"
-        self.status = "success"
-        self.description[:description] = "passed all validations!"
-      else
-        self.status = "error"
-        self.description[:description] = "failing some validations, click the 'details' link for more info."
+  def collect_results
+    lint.collect do |file, attributes|
+      attributes.collect do |attr, value|
+        value
       end
+    end.flatten!
+  end
+
+  def validation_result(result=nil)
+    case result
+    when "failure"  
+      self.status = "failure"
+      self.description[:description] = "failed all validations, click the 'details' link for more info."
+    when "success"
+      self.status = "success"
+      self.description[:description] = "passed all validations!"
+    else
+      self.status = "error"
+      self.description[:description] = "failing some validations, click the 'details' link for more info."
     end
+  end
 end
 
--- spec/models/pull_request_spec.rb
+++ spec/models/pull_request_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe PullRequest, :type => :model do
   describe "#set_status" do
     before do 
       stubbed_params = configure_pr_content
-      @pull_reques = PullRequest.new(stubbed_params)
+      @pull_request = PullRequest.new(stubbed_params)
       add_test_repo(@pull_request)
     end
     
     it "sets the status and description to error when some validations fail" do
-      pull_request.set_status
-      expect(@pull_request.status).to eq("failure")
+      @pull_request.set_status
+      expect(@pull_request.status).to eq("error")
       expect(@pull_request.description).to eq({description: "failing some validations, click the 'details' link for more info."})
     end
   
